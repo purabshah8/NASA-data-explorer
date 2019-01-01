161a12c740ce03ebfd702b1ca16f193eace4859c
Purab Shah <purabshah8@gmail.com>
2019-01-01 15:37:24 -0500

    add skeleton for DONKI

--- nasa_data_explorer.js
+++ nasa_data_explorer.js
@@ -3,20 +3,15 @@ import * as APIUtil from './api_util';
 import flatpickr from 'flatpickr';
 
 $k( () => {
-    console.log('loaded!');
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    const today = new Date();
-    // debugger
     flatpickr('#flatpickr', {
         mode: "range",
-        defaultDate: 'today'
+        defaultDate: 'today',
     });
     let searchNEOCallback = e => {
         e.preventDefault();
         const fp = $k('#flatpickr');
         const dates = fp.nodes[0].value.split(" to ");
-        fetchNEOs(dates[0], dates[1]);
+        fetchAllNEOs(dates[0], dates[1]);
         console.log('hit callback!');
     };
     APIUtil.getAPOD().then(res => {
@@ -24,26 +19,51 @@ $k( () => {
         const $apod = $k('#apod-container');
         $apod.append(`<img src="${apodURL}" class="apod"/>`);
     });
-    // fetchNEOs(dateToString(yesterday), dateToString(today));
     const $searchButton = $k('#neo-search-button');
     $searchButton.on('click', searchNEOCallback);
 });
 
-const fetchNEOs = (startDate, endDate) => {
+const fetchAllNEOs = (startDate, endDate) => {
+    if (!endDate) {
+        endDate = startDate;
+    }
+    const $neoTable = $k('.neo-table');
+    $neoTable.empty();
+    $neoTable.append(`<thead><tr class="neo-row"> 
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Closest Approach</th>
+                        <th>Velocity</th>
+                        <th>Diameter (est.)</th>
+                        <th>Potentially Hazardous?</th>
+                    </tr></thead>`);
+    const startDateArray = startDate.split('-');
+    const endDateArray = endDate.split('-');
+    const start = new Date(startDateArray[0], startDateArray[1]-1, startDateArray[2]);
+    const end = new Date(endDateArray[0], endDateArray[1]-1, endDateArray[2]);
+    if ((end-start)/86400000 > 7) {
+        let begin = new Date(start);
+        let mid = new Date(start);
+        mid.setDate(mid.getDate() + 7);
+        debugger;
+        while ((end-begin)/86400000 > 7) {
+            debugger;
+            fetchNEOs(dateToString(begin), dateToString(mid));
+            begin = new Date(mid.valueOf());
+            begin.setDate(begin.getDate() + 1);
+            mid.setDate(mid.getDate() + 7);
+            debugger;
+        }
+        fetchNEOs(dateToString(mid), endDate);
+    } else {
+        fetchNEOs(startDate, endDate);
+    }
+};
+
+function fetchNEOs(startDate, endDate) {
     APIUtil.getNEO(startDate, endDate).then(res => {
         const count = res.element_count;
-        const $neoTable = $k('.neo-table');
-        $neoTable.empty();
-        $neoTable.append(`<thead><tr class="neo-row"> 
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Closest Approach</th>
-                            <th>Velocity</th>
-                            <th>Diameter (est.)</th>
-                            <th>Potentially Hazardous?</th>
-                        </tr></thead>`);
         const dates = Object.values(res.near_earth_objects);
-        console.log(count, dates);
         dates.forEach(date => {
             date.forEach(neo => {
                 const closeApproachData = neo.close_approach_data[0];
@@ -56,6 +76,7 @@ const fetchNEOs = (startDate, endDate) => {
                 closestApproach = addCommas(closestApproach);
                 const velocity = closeApproachData.relative_velocity.kilometers_per_second;
                 const hazardous = neo.is_potentially_hazardous_asteroid ? "Yes" : "No";
+                const $neoTable = $k('.neo-table');
                 $neoTable.append(`<tr class="neo-row">
                                     <td>${neo.name}</td>
                                     <td>${closeApproachData.close_approach_date}</td>
@@ -67,7 +88,7 @@ const fetchNEOs = (startDate, endDate) => {
             });
         });
     });
-};
+}
 
 function dateToString(date) {
     return date.toJSON().split('T')[0];
